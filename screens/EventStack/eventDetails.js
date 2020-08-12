adf81c0a4d87b6d6f432d94212d7c16681e3604e
alantjose <f20180187@goa.bits-pilani.ac.in>
2020-08-12 17:18:25 +0530

    even ui

--- screens/EventStack/eventDetails.js
+++ screens/EventStack/eventDetails.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
 import { t } from "react-native-tailwindcss";
+import { Entypo, MaterialCommunityIcons } from "@expo/vector-icons";
+import { Card } from "@paraboly/react-native-card";
+import moment from "moment";
 
 const EventDetails = ({ route }) => {
   // console.log("hello");
   const { name, location, date, time } = route.params;
+
   return (
     <View
       style={[
@@ -15,15 +19,51 @@ const EventDetails = ({ route }) => {
         t.mX2,
         t.mT10,
         t.mB20,
-        t.bgGray100,
+        t.bgWhite,
+        // t.roundedFull,
+        t.shadowXl,
         t.rounded,
         t.pY10,
       ]}
     >
-      <View style={[t.textCenter, t.pB10, t.flexCol, t.itemsCenter]}>
-        <Text style={[t.text5xl, t.textCenter, t.pY2]}>{name}</Text>
-        <Text style={[t.textXl, t.textCenter]}>At {location}</Text>
-        <Text style={[t.textXl, t.textCenter]}>{`On ${date} at ${time}`}</Text>
+      <View style={[]}>
+        <View>
+          <Entypo
+            name="bell"
+            size={64}
+            color="#e80e5a"
+            style={[t.mT4, t.textCenter, t.pY2]}
+          />
+        </View>
+
+        <Text style={[t.text5xl, t.mY3, t.textCenter]}>{name}</Text>
+
+        <View style={[]}>
+          <View style={[t.flex, t.flexRow, t.pY2, t.mY1]}>
+            <Entypo name="pin" size={32} color="grey" style={[t.mX2]} />
+
+            <Text style={[t.textXl]}>{location}</Text>
+          </View>
+
+          <View style={[t.flex, t.flexRow, t.pY2, t.mY1]}>
+            <Entypo name="calendar" size={32} color="grey" style={[t.mX2]} />
+
+            <Text style={[t.textXl]}>
+              {moment(date).format("dddd, MMMM Do YYYY ")}
+            </Text>
+          </View>
+
+          <View style={[t.flex, t.flexRow, t.pY2, t.mY1]}>
+            <MaterialCommunityIcons
+              name="clock-outline"
+              size={32}
+              color="grey"
+              style={[t.mX2]}
+            />
+
+            <Text style={[t.textXl]}>{time}</Text>
+          </View>
+        </View>
       </View>
     </View>
   );
